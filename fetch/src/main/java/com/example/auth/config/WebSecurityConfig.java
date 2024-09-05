b13ea2b9d9c6d574ce1c301cd7a7e890403f17f7
edujeeho <edujeeho@gmail.com>
2024-09-05 09:12:07 +0900

    other settings

--- fetch/src/main/java/com/example/auth/config/WebSecurityConfig.java
+++ fetch/src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/error", "/token/issue", "/views/**")
+                    auth.requestMatchers("/error", "/token/issue", "/views/**", "/static/**")
                             .permitAll();
                     auth.requestMatchers("/articles/**")
                             .permitAll();
